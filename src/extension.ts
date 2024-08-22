d4c2c0490771086f0a6c9145c37a12160763dbf6
Sylvain Nieuwlandt <nieuwlandt.s@an0rak.dev>
2024-08-22 16:08:45 +0200

    Request pkg.go.dev and parse the resulting html

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,39 @@
 import * as vscode from 'vscode';
+import parse from 'node-html-parser';
+
+interface Package {
+	name: string;
+	import: string;
+	synopsis: string;
+};
 
 async function search(input : string) {
 	const got = (await import("got")).got;
-	const uri = encodeURI(`https://pkg.go.dev/search?q=${input}`);
+	const uri = encodeURI(`https://pkg.go.dev/search?q=${input}&limit=100`);
+	console.debug(`Searching a Go package documentation for ${input}`)
 	const req = await got.get(uri);
 	return req.body;
+}
+
+function transformToPackage(rawHtml: string) : Package[] {
+	const dom = parse(rawHtml);
+	const searchResults = dom.querySelectorAll('.SearchSnippet');
+	console.debug(`Found ${searchResults.length} results for this search.`)
+	let result : Package[] = [];
+	for (var searchResult of searchResults) {
+		const header = searchResult.querySelector('a')?.innerText;
+		if (!header) {
+			console.warn("No header found in search result, skipping it.")
+			continue;
+		}
+		const sanitizedHeader = header.replace(/(\n|\s\s+)/gm, "").split('(');
+		result = result.concat({
+			name: sanitizedHeader[0],
+			import: sanitizedHeader[1].substring(0, sanitizedHeader[1].length-1),
+			synopsis: searchResult.querySelector('p')?.innerText || ""
+		});
+	}
+	return result;
 }
 
 export function activate(context: vscode.ExtensionContext) { 
@@ -14,8 +43,9 @@ export function activate(context: vscode.ExtensionContext) {
 		console.debug("Searching for package");
 		const searchInput = await vscode.window.showInputBox({title: "Pkg-go-dev: Search", prompt:"Search packages or symbols"});
 		if (searchInput) {
-			const result = await search(searchInput);
-			console.debug({r: result});
+			const rawHtml = await search(searchInput);
+			const searchResults = transformToPackage(rawHtml);
+			vscode.window.showQuickPick(searchResults.map(r => { return {label: r.name, description: r.import, detail: r.synopsis } }));
 		}
 	});
 
