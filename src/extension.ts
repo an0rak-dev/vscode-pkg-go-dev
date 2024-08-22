329f92529c8c9a62f22573f153a9209cc45b0288
Sylvain Nieuwlandt <nieuwlandt.s@an0rak.dev>
2024-08-22 12:40:43 +0200

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,24 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
+async function search(input : string) {
+	const got = (await import("got")).got;
+	const uri = encodeURI(`https://pkg.go.dev/search?q=${input}`);
+	const req = await got.get(uri);
+	return req.body;
+}
+
+export function activate(context: vscode.ExtensionContext) { 
 	console.log("Activating extension pkg-go-dev");
 
-	const searchCommand = vscode.commands.registerCommand('pkg-go-dev.search', () => {
+	const searchCommand = vscode.commands.registerCommand('pkg-go-dev.search', async () => {
 		console.debug("Searching for package");
+		const searchInput = await vscode.window.showInputBox({title: "Pkg-go-dev: Search", prompt:"Search packages or symbols"});
+		if (searchInput) {
+			const result = await search(searchInput);
+			console.debug({r: result});
+		}
 	});
+
 
 	context.subscriptions.push(searchCommand);
 }
